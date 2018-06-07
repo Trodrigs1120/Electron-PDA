74ece25af2d0713bdaf636e57adf4a729cb9899a
unknown <Trodrigs@yahoo.com>
2018-06-07 14:37:51 -0400

    first commit

---
+++ assets/javascript.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(".to-do-items").append('<p> Take a shower</p>');
+    // here we'll append all the daily to do items
+    
+    
+    // now we create a button in order to make the form appear
+})
