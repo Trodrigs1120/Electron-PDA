4ac6ea2e778094556f60f34d8647aac2d7dbc0c4
Taylor Rodrigs <Trodrigs@Yahoo.com>
2018-06-14 16:30:34 -0400

    Add files via upload

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,23 +1,98 @@
+electron = require('electron')
+fs = electron.remote.require('fs')
+
+// we'll go ahead and write everything into json after we finish inputting
+
 $(document).ready(function () {
-    $(".to-do-items").append('<p> Take a shower</p>');
+    let activities                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+     taskintake()
+    
+     function taskintake(){
+        
+        fs.readFile('activities.json', (err, data) => {  
+            if (err) throw err;
+            console.log(data)
+             activities = JSON.parse(data);
+             console.log(activities);
+            // console.log(activities[0]);
+            // console.log(activities[0].task)
+            
+            //appending tasks to their tables
+            for (let i =0; i<activities.length; i++) {
+                switch(activities[i].time) {
+                    case "Daily":
+					//we will need to rewrite this for the loop once i know it works
+                     $("#Daily").append("<tr><td>" + activities[i].activity + "</td><td>" + activities[i].task + "</td>");
+                    break;
+					
+                    case "Weekly":
+                    $("#Weekly").append("<tr><td>" + activities[i].activity + "</td><td>" + activities[i].task + "</td>");
+                   
+					break;
+					case "Monthly":
+                    $("#Monthly").append("<tr><td>" + activities[i].activity + "</td><td>" + activities[i].task + "</td>");
+                    
+					break;
+                }
+            }
+            
+        });
+        
+        console.log('This is after the read call');  
+        //probably going to be putting this in a loop when we have more than 1 item
+        console.log(activities);
+
+     }
+     
+
+
+         
+
+
+
+
+    // $(".to-do-items").append('<p> Take a shower</p>');
     // here we'll append all the daily to do items
-    
-    
+        
     // now we create a button in order to make the form appear
     Button=$("<button>Add Item</button>");
     Button.addClass("add-for-today")
-    $(".to-do-items").append(Button);
-
+    $(".menu-buttons").append(Button);
+	
 
     // const multiplyES6 = (x, y) => { return x * y };
     $(".add-for-today").on("click", function(){
+        
  // now we add the form for a new event
- $(".to-do-items").append('<form id="mySearch"> <input type="radio" name="time" value="today" checked> Today<br> <input type="radio" name="time" value="week" checked> This Week<br> <input type="radio" name="time" value="month" checked> This Month<br> <input id="repeating" type="checkbox"> Repeating? <br>  Task name: <input type="text" name="task-name"><br> Details: <input type="text" name="task-details"><br> </form>'); //append a new form element with id mySearch to <body>
-
+ $(".menu-buttons").append('<form id="my-input"> <input type="radio" name="time" value="today" checked> Today<br> <input type="radio" name="time" value="week" checked> This Week<br> <input type="radio" name="time" value="month" checked> This Month<br> <input id="repeating" type="checkbox"> Repeating? <br>  Task name: <input type="text" name="task-name" id="task-name"><br> Details: <input type="text" name="task-details" id="task-details"><br> <button type="submit" form="my-input" id="submit" value="Submit">Submit</button></form>'); //append a new form element with id mySearch to <body>
+// the submit button needs to hide the added buttons and form fields,
+// we might want to disable the add item button or at least hide it until we're gonna add more stuff
 console.log("event fired")
     } )
 
-//  $(".add-for-today").onclick = AddForm
+    $("#submit").on("click", function() {
+        let Activity = $("#task-name").val().trim();
+        let Details = $("#task-details").val().trim();
+    
+        let NewTask = {
+           Activity : Activity,
+           Details: Details,
+        }
+        console.log (NewTask.Activity)
+        console.log (NewTask.Details)
+        let test=(JSON.stringify(NewTask))
+        alert(NewTask.Activity)
+        // $("#myTable").append("<tr><td>" + NewTask.Activity + "</td><td>" + NewTask.Details + "</td>");
+        fs.appendFile("./test.txt", (test), (err) => {
+            if (err) throw err;
+            console.log('It\'s saved!');
+        });
+    })
 
 })
+
+// let TestTimer = setInterval(myTimer, 1000000); // the timer should be approximately an hour. It'll check for new records every hour
+// function myTimer() { 
+//     alert("Hello! I am an alert box!!");
+// }
 
