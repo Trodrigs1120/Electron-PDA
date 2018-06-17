17dd14b44e7136012554c1e8d9ec680987c7eca1
Taylor Rodrigs <Trodrigs@Yahoo.com>
2018-06-16 21:36:10 -0400

    working on local storage solution
    a for loop won't cut it due to the async nature will set it up to run recursively

--- assets/javascript.js
+++ assets/javascript.js
@@ -3,90 +3,113 @@ fs = electron.remote.require('fs')
 
 // we'll go ahead and write everything into json after we finish inputting
 
-$(document).ready(function () {
-    let activities                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
-     taskintake()
+$(document).ready(function() {
+    let activities
+    let ToWritetoText
+    taskintake()
+
     
-     function taskintake(){
-        
-        fs.readFile('activities.json', (err, data) => {  
+    function taskintake() {
+
+        fs.readFile('activities.json', (err, data) => {
             if (err) throw err;
             console.log(data)
-             activities = JSON.parse(data);
-             console.log(activities);
-            // console.log(activities[0]);
+            activities = JSON.parse(data);
+            console.log(activities);
+            ToWritetoText = activities
+            // console.lhbog(activities[0]);
             // console.log(activities[0].task)
-            
+
             //appending tasks to their tables
-            for (let i =0; i<activities.length; i++) {
-                switch(activities[i].time) {
+            for (let i = 0; i < activities.length; i++) {
+                switch (activities[i].time) {
                     case "Daily":
-					//we will need to rewrite this for the loop once i know it works
-                     $("#Daily").append("<tr><td>" + activities[i].activity + "</td><td>" + activities[i].task + "</td>");
-                    break;
-					
+                        //we will need to rewrite this for the loop once i know it works
+                        $("#Daily").append("<tr><td>" + activities[i].activity + "</td><td>" + activities[i].task + "</td>");
+                        break;
+
                     case "Weekly":
-                    $("#Weekly").append("<tr><td>" + activities[i].activity + "</td><td>" + activities[i].task + "</td>");
-                   
-					break;
-					case "Monthly":
-                    $("#Monthly").append("<tr><td>" + activities[i].activity + "</td><td>" + activities[i].task + "</td>");
-                    
-					break;
+                        $("#Weekly").append("<tr><td>" + activities[i].activity + "</td><td>" + activities[i].task + "</td>");
+
+                        break;
+                    case "Monthly":
+                        $("#Monthly").append("<tr><td>" + activities[i].activity + "</td><td>" + activities[i].task + "</td>");
+
+                        break;
                 }
             }
-            
+
         });
-        
-        console.log('This is after the read call');  
+
+        console.log('This is after the read call');
         //probably going to be putting this in a loop when we have more than 1 item
         console.log(activities);
 
-     }
-     
-
-
-         
+    }
+   
 
 
 
 
     // $(".to-do-items").append('<p> Take a shower</p>');
     // here we'll append all the daily to do items
-        
+
     // now we create a button in order to make the form appear
-    Button=$("<button>Add Item</button>");
+    Button = $("<button>Add Item</button>");
     Button.addClass("add-for-today")
     $(".menu-buttons").append(Button);
-	
+
 
     // const multiplyES6 = (x, y) => { return x * y };
-    $(".add-for-today").on("click", function(){
-        
- // now we add the form for a new event
- $(".menu-buttons").append('<form id="my-input"> <input type="radio" name="time" value="today" checked> Today<br> <input type="radio" name="time" value="week" checked> This Week<br> <input type="radio" name="time" value="month" checked> This Month<br> <input id="repeating" type="checkbox"> Repeating? <br>  Task name: <input type="text" name="task-name" id="task-name"><br> Details: <input type="text" name="task-details" id="task-details"><br> <button type="submit" form="my-input" id="submit" value="Submit">Submit</button></form>'); //append a new form element with id mySearch to <body>
-// the submit button needs to hide the added buttons and form fields,
-// we might want to disable the add item button or at least hide it until we're gonna add more stuff
-console.log("event fired")
-    } )
+    $(".add-for-today").on("click", function() {
+
+        // now we add the form for a new event
+        $(".menu-buttons").append('<form id="my-input"> <input type="radio" name="time" value="today" checked> Today<br> <input type="radio" name="time" value="week" checked> This Week<br> <input type="radio" name="time" value="month" checked> This Month<br> <input id="repeating" type="checkbox"> Repeating? <br>  Task name: <input type="text" name="task-name" id="task-name"><br> Details: <input type="text" name="task-details" id="task-details"><br> <button type="submit" form="my-input" id="submit" value="Submit">Submit</button></form>'); //append a new form element with id mySearch to <body>
+        // the submit button needs to hide the added buttons and form fields,
+        // we might want to disable the add item button or at least hide it until we're gonna add more stuff
+        console.log("event fired")
+        console.log(ToWritetoText)
+    })
 
     $("#submit").on("click", function() {
-        let Activity = $("#task-name").val().trim();
-        let Details = $("#task-details").val().trim();
-    
-        let NewTask = {
-           Activity : Activity,
-           Details: Details,
-        }
-        console.log (NewTask.Activity)
-        console.log (NewTask.Details)
-        let test=(JSON.stringify(NewTask))
-        alert(NewTask.Activity)
+        // let Activity = $("#task-name").val().trim();
+        // let Details = $("#task-details").val().trim();
+
+        // let NewTask = {
+        //     Activity: Activity,
+        //     Details: Details,
+        // }
+
+        //  let activities = (JSON.stringify(activities))
+        console.log(activities[0])
+        console.log(activities[1])
+        console.log(activities[2])
+        // alert(NewTask.Activity)
         // $("#myTable").append("<tr><td>" + NewTask.Activity + "</td><td>" + NewTask.Details + "</td>");
-        fs.appendFile("./test.txt", (test), (err) => {
-            if (err) throw err;
-            console.log('It\'s saved!');
-        });
+        
+        //Switch statement, if i=0 we start it with [ if i=activities.length we end it in ]
+
+        // think I need to use recursion for this to write successfully
+        //something like increment it like a for loop, but just calling it back to the function itself until its exit condition is met.
+         for(let i=0; i<activities.length; i++){
+            if (i=0){
+                fs.writeFile("./test.txt", ("["), (err) => {
+                    if (err) throw err;
+                   console.log('prepended');
+               });
+            }
+            fs.appendFile("./test.txt", ('{"time":' +'"'+ activities[i].time+'"'+","+'"activity":' +'"'+ activities[i].activity+'"'+","+ '"task":'+'"'+ activities[i].task +'"'+'}'+","), (err) => {
+                if (err) throw err;
+               console.log('It\'s saved!');
+           });
+           if (i=activities.length) {
+            fs.appendFile("./test.txt", ("]"), (err) => {
+                if (err) throw err;
+               console.log('It\'s saved!');
+           });
+           }
+         }
+        
     })
 
 })
@@ -95,4 +118,3 @@ console.log("event fired")
 // function myTimer() { 
 //     alert("Hello! I am an alert box!!");
 // }
-
